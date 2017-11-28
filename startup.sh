b263353d409b1f867f5e699d619c8f18414de9f1
Benjamin Campbell <ben@polar.digital>
2017-11-28 17:51:14 +0000

    fixing

--- startup.sh
+++ startup.sh
@@ -7,8 +7,14 @@ echo "Script Started!"
 # Get root up in here
 sudo su
 
+echo "sudo ran"
+
 #install git
 sudo apt-get install git
+
+echo "git installed"
+
+echo "installing node"
 
 # Change to what ever version you want here see link to versions below
 sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
